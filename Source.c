36089b73f6aa4e16e8ae65e0414d2a1ee74d5648
ghduf3 <109141717+ghduf3@users.noreply.github.com>
2022-09-21 21:47:52 +0900

    2022.09.21(문자열 함수)

--- Source.c
+++ Source.c
@@ -1,71 +1,62 @@
 ﻿#include <stdio.h>
-#include <string.h> // 문자열 관련 함수
+#include <string.h>
 
-// 자기 참조 구조체
-/*
-struct Node
+void Mystrcat(char * str1, const char * str2)
 {
-	int data;
-	struct Node * pointer; // <- 구조체를 가리킬 수 있는 포인터 변수
-};
-*/
+	// str1이 NULL문자가 아니라면
+	while (*str1 != '\0')
+	{
+		// str1이 가리키는 포인터를 증가시킵니다.
+		str1++;
+	}
+
+	while (*str2 != '\0')
+	{
+		*str1 = *str2;
+		str1++;
+		str2++;
+	}
+	
+}
 
 int main()
 {
-	// 자기 참조 구조체란?
+	// strcmp 문자열 비교 함수
 	/*
-	// 자신과 동일한 구조체를 가리킬 수 있는 포인터 변수를
-	// 멤버 변수로 가지는 구조체입니다.
-	struct Node node1 = { 10, NULL};
-	struct Node node2 = { 20, NULL };
-	struct Node node3 = { 30, NULL };
-	struct Node * structPtr = &node1;
-	printf("첫 번째 구조체 data의 값 : %d\n", structPtr->data);
-	structPtr->pointer = &node2;
-	printf("두 번째 구조체 data의 값 : %d\n", structPtr->pointer->data);
-	structPtr->pointer->pointer = &node3;
-	printf("세 번째 구조체 data의 값 : %d\n", structPtr->pointer->pointer->data);
+	// ABCD vs ABTT
+
+	char memory1[] = { "ABCE" };
+	char memory2[] = { "ABCT" };
+	char memory3[] = { "ABCC" };
+
+	// strcmp는 첫번째 문자여르이 크기가 크다면 양수(+1)을 반환합니다.
+	// 그런데 두번째 문자열의 크기가 크다면 음수(-1)을 반환합니다.
+	// 만약에 두 개다 문자열의 크기가 같다면 0을 반환합니다.
+	printf(" 두 개의 문자열을 비교한 값 : %d\n", strcmp(memory1, memory2));
+	printf(" 두 개의 문자열을 비교한 값 : %d\n", strcmp(memory1, memory3));
+
+	// strcmp 영어 사전 순서로 우선 순위가 결정됩니다.
+
+	// strncmp : 첫번째 매개변수에 비교할 문자열
+	// strncmp : 두번째 매개변수에 비교할 문자열
+	// strncmp : 세번째 비교할 문자열의 길이
+
+	// ABC == ABC = 0
+	strcmp(memory1, memory2, 5);
+	printf(" 두 개의 문자열을 비교한 값 : %d\n", strncmp(memory1, memory2, 3));
 	*/
 
-	// 문자열 함수	
-	// strlen - (문자열의 길이를 출력하는 함수)
-	char name[] = { "Bard" };
-	printf("name의 값 : %s\n", name);
+	// strcat 문자열 연결 함수
+	char name1[10] = { "Kim" };
+	char name2[] = { "Geum" };
 
-	// strlen( )함수는 문자열의 길이를 구할 때 NULL문자 이전의 문자열 길이만 계산합니다.
-	printf("name배열의 길이 : %d\n", strlen(name));
+	// strcat같은 경우에는 복사받을 문자열의 크기를 넉넉히 지정해주어야 합니다.
+	// strcat_s(name1, 10, name2);
+	// printf("name1의 문자열 : %s\n", name1);
 
-	// strcpy - (문자열을 복사하는 함수)
-	char A[10] = { "LEAGUE" };
-	char B[10] = { "LEGEND" };
+	Mystrcat(name1, name2);
 
-	// read only 영역에 있는 문자열 리터럴이기 때문에 값을 변경할 수 없습니다.
-	const char* C = "Player";
-	const char* D = "Monster";
-
-	/*
-	// A 와 B는 문자열의 시작주소를 의미합니다.
-	// 00FF44D0(A) <- 00DD22A0(B)
-	int i = 0;
-	while (A[i]) // NULL문자는 0을 의미하기 때문에
-	{
-		A[i] = B[i];
-		i++;
-	} // while문의 값이 false로 변경됩니다.
-	printf("A의 문자열은 : %s", A);
-	*/
-
-	// strcpy( )함수의 첫 번째 매개변수는 복사받을 문자열입니다.
-	// strcpy( )함수의 두 번째 매개변수는 복사할 문자열입니다.
-
-	// strcpy_s()함수의 첫 번째 매개변수는 복사받을 문자열입니다.
-	// strcpy_s()함수의 두 번째 매개변수는 복사할 메모리 크기입니다.
-	// strcpy_s( )함수의 세 번째 매개변수는 복사할 문자열입니다.
-	strcpy_s(A, 10, B);
-	printf("복사한 A 문자열의 값 : %s", A);
-
-	// strcmp - (문자열을 비교하는 함수)
-	// strcat - (문자열을 연결하는 함수)
+	printf("%s", name1);
 
 	return 0;
 }
